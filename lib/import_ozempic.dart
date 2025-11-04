e04d2bf57b66881f8d9fa3b51de6dfb276ae2e97
mrgnhnt96 <>
2025-11-04 11:23:46 -0700

    feat: add version flag

--- lib/import_ozempic.dart
+++ lib/import_ozempic.dart
@@ -2,6 +2,7 @@ import 'package:import_ozempic/commands/fix_command.dart';
 import 'package:import_ozempic/commands/update_command.dart';
 import 'package:import_ozempic/deps/log.dart';
 import 'package:import_ozempic/domain/args.dart';
+import 'package:import_ozempic/gen/version.dart';
 
 const _usage = '''
 Usage: import_ozempic <command> <arguments>
@@ -9,12 +10,21 @@ Usage: import_ozempic <command> <arguments>
 Commands:
   fix <files...>   Fix the imports in the given files
   update           Update the package to the latest version
+
+
+Flags:
+  --version        Print the version of the package
 ''';
 
 class ImportOzempic {
   const ImportOzempic();
 
   Future<int> run(Args args) async {
+    if (args['version'] case true) {
+      log(version);
+      return 0;
+    }
+
     int exitCode = 0;
     switch (args.path) {
       case ['fix', ...final files]:
