1d4a090b9b52fddb915a6c36a99fc44b57e2a528
mrgnhnt96 <>
2025-11-04 11:25:43 -0700

    feat: add check for latest version post execution

--- lib/import_ozempic.dart
+++ lib/import_ozempic.dart
@@ -1,5 +1,6 @@
 import 'package:import_ozempic/commands/fix_command.dart';
 import 'package:import_ozempic/commands/update_command.dart';
+import 'package:import_ozempic/deps/is_up_to_date.dart';
 import 'package:import_ozempic/deps/log.dart';
 import 'package:import_ozempic/domain/args.dart';
 import 'package:import_ozempic/gen/version.dart';
@@ -20,20 +21,27 @@ class ImportOzempic {
   const ImportOzempic();
 
   Future<int> run(Args args) async {
+    int exitCode = 0;
+
     if (args['version'] case true) {
       log(version);
-      return 0;
+    } else {
+      switch (args.path) {
+        case ['fix', ...final files]:
+          exitCode = await FixCommand(args: args).run(files);
+        case ['update']:
+          exitCode = await UpdateCommand().run();
+        default:
+          log(_usage);
+          exitCode = 1;
+      }
     }
 
-    int exitCode = 0;
-    switch (args.path) {
-      case ['fix', ...final files]:
-        exitCode = await FixCommand(args: args).run(files);
-      case ['update']:
-        exitCode = await UpdateCommand().run();
-      default:
-        log(_usage);
-        exitCode = 1;
+    if (!await isUpToDate.check()) {
+      final latestVersion = await isUpToDate.latestVersion();
+      log(
+        'A new version is available ($latestVersion). Run `import_ozempic update` to update.',
+      );
     }
 
     return exitCode;
